ea6b2ca4a5c15e7fa95cbf21fe5e950829497a3a
BalasundaramVi <vigneshbalasundaram@gmail.com>
2019-02-11 17:07:41 -0800

    Finalize styling and HTML for Home component

---
+++ pizza-planet/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="row">
+    <nav class="navbar navbar-toggleable-md navbar-light">
+      <a class="navbar-brand" href="/">PIZZA PLANET</a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">MENU</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+header {
+  margin-bottom: 20px;
+}
+
+.navbar-brand {
+  font-size: 1.5em;
+}
+
+.navbar-nav {
+  flex-direction: row;
+}
+
+.nav-item {
+  padding: 5px;
+}
+
+</style>
